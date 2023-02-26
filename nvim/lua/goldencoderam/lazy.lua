7e16670d6539c8e414044f2ffd160afdad6f6091
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-02-25 20:21:18 -0500

    feat(nvim): change Packer for Lazy

--- nvim/lua/goldencoderam/lazy.lua
+++ nvim/lua/goldencoderam/lazy.lua
@@ -1,24 +1,49 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Initialize lazy
-require("lazy").setup(plugins, opts)
 
 -- example using a list of specs with the default options
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-  "folke/neodev.nvim",
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+            })
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
+    {
+        "folke/which-key.nvim",
+        lazy = true
+    },
+    {
+        -- Example for neo-tree.nvim
+        "nvim-neo-tree/neo-tree.nvim",
+        keys = {
+            { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+        },
+        dependencies = { 
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("neo-tree").setup()
+        end,
+    }
 })
