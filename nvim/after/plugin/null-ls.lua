b7d7c06c7639fdf247388d0014e42506cdbfa1dd
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-02-28 07:49:33 -0500

    feat(nvim): new lsp, remaps and other config

---
+++ nvim/after/plugin/null-ls.lua
@@ -0,0 +1,12 @@
+local null_ls = require("null-ls")
+
+local sources = {
+    null_ls.builtins.completion.luasnip,
+
+    -- Formatting
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.prettier,
+}
+
+null_ls.setup({ sources = sources })
