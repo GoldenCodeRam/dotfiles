5040014396a618fa7dbaee66fc835f3c76d4ac47
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-05-14 17:48:49 -0500

    feat: mason-nullls configuration

--- nvim/lua/goldencoderam/lazy.lua
+++ nvim/lua/goldencoderam/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -15,146 +15,164 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
 
 require("lazy").setup({
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
-				transparent = true,
-			})
-			-- Lua
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
-	{
-		-- This I feel is needed, as I sometimes forget the keys. This might
-		-- be disabled later, but I like it.
-		"folke/which-key.nvim",
-		lazy = true,
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			-- disable netrw at the very start of your init.lua (strongly advised)
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("kanagawa").setup({
+                transparent = true,
+            })
+            -- Lua
+            vim.cmd([[colorscheme kanagawa]])
+        end,
+    },
+    {
+        -- This I feel is needed, as I sometimes forget the keys. This might
+        -- be disabled later, but I like it.
+        "folke/which-key.nvim",
+        lazy = true,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            -- disable netrw at the very start of your init.lua (strongly advised)
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
 
-			-- set termguicolors to enable highlight groups
-			vim.opt.termguicolors = true
+            -- set termguicolors to enable highlight groups
+            vim.opt.termguicolors = true
 
-			-- empty setup using defaults
-			require("nvim-tree").setup()
-		end,
-	},
-	{
-		"mbbill/undotree",
-		lazy = false,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		build = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				highlight = {
-					enable = true,
-				},
-			})
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		lazy = false,
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
-	{
-		"tpope/vim-fugitive",
-	},
-	{
-		-- LSP configuration
-		"neovim/nvim-lspconfig",
-		lazy = true,
-		dependencies = {
-			-- LSP support
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-	},
-	{
-		-- Autocompletion
-		"hrsh7th/nvim-cmp",
-		lazy = false,
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			-- Snippets
-			--"hrsh7th/cmp-vsnip",
-			--"hrsh7th/vim-vsnip",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-		},
-	},
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		lazy = true,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		-- or                              , branch = '0.1.1',
-		dependencies = { "nvim-lua/plenary.nvim" },
-		lazy = true,
-	},
-	{
-		-- Notifications, pretty cool.
-		"folke/noice.nvim",
-		lazy = false,
-		config = function()
-			-- Suggested setup.
-			require("noice").setup({
-				lsp = {
-					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
-					},
-				},
-				-- you can enable a preset for easier configuration
-				presets = {
-					bottom_search = true, -- use a classic bottom cmdline for search
-					command_palette = true, -- position the cmdline and popupmenu together
-					long_message_to_split = true, -- long messages will be sent to a split
-					inc_rename = false, -- enables an input dialog for inc-rename.nvim
-					lsp_doc_border = false, -- add a border to hover docs and signature help
-				},
-			})
-		end,
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			-- This one is optional.
-			-- "rcarriga/nvim-notify",
-		},
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		lazy = false,
-		config = function()
-			require("lualine").setup({})
-		end,
-	},
+            -- empty setup using defaults
+            require("nvim-tree").setup()
+        end,
+    },
+    {
+        "mbbill/undotree",
+        lazy = false,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })
+        end,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+    {
+        "windwp/nvim-autopairs",
+        lazy = false,
+        config = function()
+            require("nvim-autopairs").setup({})
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        -- LSP configuration
+        "neovim/nvim-lspconfig",
+        lazy = true,
+        dependencies = {
+            -- LSP support
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+    },
+    {
+        -- Autocompletion
+        "hrsh7th/nvim-cmp",
+        lazy = false,
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            -- Snippets
+            --"hrsh7th/cmp-vsnip",
+            --"hrsh7th/vim-vsnip",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        lazy = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "jose-elias-alvarez/null-ls.nvim",
+        },
+    },
+    {
+        "folke/trouble.nvim",
+        lazy = true,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("trouble").setup({})
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        -- or                              , branch = '0.1.1',
+        dependencies = { "nvim-lua/plenary.nvim" },
+        lazy = true,
+    },
+    {
+        -- Notifications, pretty cool.
+        "folke/noice.nvim",
+        lazy = false,
+        config = function()
+            -- Suggested setup.
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true,
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true,         -- use a classic bottom cmdline for search
+                    command_palette = true,       -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false,       -- add a border to hover docs and signature help
+                },
+            })
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            -- This one is optional.
+            -- "rcarriga/nvim-notify",
+        },
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        lazy = false,
+        config = function()
+            require("lualine").setup({})
+        end,
+    },
 })
